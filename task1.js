69f2e77d3e9f4b524a0648452dee036c02272439
Gino Gains G <ginognanadurai@gmail.com>
2026-08-25 23:52:54 +0530

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,23 @@
+let studentName = "Alex";
+let studentAge = 20;
+let studentMark = 95;
+
+console.log(studentName);
+console.log(studentAge);
+console.log(studentMark);
+
+
+
+let userName = prompt("Enter your name:");
+alert("Hello, " + userName);
+
+
+let userAge = prompt("Enter your age:");
+console.log(userAge);
+
+
+let isConfirmed = confirm("Do you want to continue?");
+
+
+let name = prompt("Enter your name:");
+document.writeln("Welcome, " + name);
